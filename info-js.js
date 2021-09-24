36299b8a9d41a5b81bd759df2465cd1b093a1bb2
lorenapassagem <lorenapassagem@gmail.com>
2021-09-24 10:24:39 -0300

    finished the feature show description

--- info-js.js
+++ info-js.js
@@ -1,6 +1,9 @@
 const showInfo = () => {
     console.log("pequei o clique")
     const description = document.querySelector(".description")
-    console.log(description)
+    description.classList.add("-showdDescription")
+    
+    const photoCard = document.querySelector(".photo-card")
+    photoCard.classList.add("-open")
 }
 
